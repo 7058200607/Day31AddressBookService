5d3702461aa1b09a3755847176c7b63343c6341b
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-08-26 23:03:22 +0530

    insert new contact in addres book service

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,3 +6,9 @@ use Address_Book_service;
 create table Address_Book(FName varchar(200),LName varchar(200),Address varchar(200),City varchar(200),State varchar(200),Zip varchar(200),PhoneNumber varchar(200),email varchar(200));
 ---UC3
 select* from Address_Book;
+---UC4
+insert into Address_Book values('vrushali','Joshi','PundlikNagar','aurangabad','MH',431001,7820060781,'vrushalijoshi589@gmai.com');
+insert into Address_Book values('Mrunali','Pawar','Garkheda','aurangabd','MH',431001,9825060781,'mrunalipawar123@gmai.com');
+insert into Address_Book values('Punam','patil','samarthNagar','aurangabad','MH',431001,8920060781,'punampatil896@gmai.com');
+insert into Address_Book values('saurabh','patil','cidco','aurangabad','MH',431001,9620060781,'saurabhp59@gmai.com');
+insert into Address_Book values('ajay','wagh','cidco','aurangabad','MH',431001,7020060781,'ajaywagh9@gmai.com');
