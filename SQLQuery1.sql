9584b505de8618f02e99b27f01d763bc743dcbd3
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-08-26 22:54:55 +0530

    create a table in database with datails

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,6 @@
 ---create address Book Service
 --UC1
 create database Address_Book_service;
+---UC2
+use Address_Book_service;
+create table Address_Book(FName varchar(200),LName varchar(200),Address varchar(200),City varchar(200),State varchar(200),Zip varchar(200),PhoneNumber varchar(200),email varchar(200));
