17f045980345c04c85a5f01041985c49cef85126
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-08-26 22:58:05 +0530

    Insert a new adress in address book service

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,3 +4,5 @@ create database Address_Book_service;
 ---UC2
 use Address_Book_service;
 create table Address_Book(FName varchar(200),LName varchar(200),Address varchar(200),City varchar(200),State varchar(200),Zip varchar(200),PhoneNumber varchar(200),email varchar(200));
+---UC3
+select* from Address_Book;
